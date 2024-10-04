0a3dcc40ec2a17c1e858605e30fe53ea1934d6b2
Valentin Alberto <valalberto0214@gmail.com>
2024-10-04 10:23:35 +0800

    initial react tic tac toe gameboard

---
+++ react-tictactoe/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = React.useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
+
+    if (isEditing) {
+        editablePlayerName = (
+            <input className='player-name' type='text' required value={playerName} onChange={handleChange} />
+        );
+        // btnCaption = 'Save';
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+                <button onClick={handleEditClick}>{isEditing ?'Save':'Edit'}</button>
+            </span>
+        </li>
+    );
+}
