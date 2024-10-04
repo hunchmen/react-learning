5f7ea850ea4999f4192641509628ba4154ac5610
Valentin Alberto <valalberto0214@gmail.com>
2024-10-04 22:59:40 +0800

    ep86 Sharing state  across components

--- react-tictactoe/src/components/Gameboard.jsx
+++ react-tictactoe/src/components/Gameboard.jsx
@@ -1,35 +1,43 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
-export default function Gameboard({onSelectSquare, activePlayerSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function Gameboard({ onSelectSquare, turns }) {
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
 
-        onSelectSquare();
+    //     onSelectSquare();
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
     }
 
     return (
         <ol id='game-board'>
-            {gameBoard.map((row, rowIndex) => 
-            <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => 
-                    <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
-                    </li>)}
-                </ol>
-            </li>)}
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
         </ol>
     );
 }
---
+++ react-tictactoe/src/components/Log.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+export default function Log({turns}) {
+    return(
+        <ol id="log">
+            {turns.map(turn => 
+            <li key={`${turn.square.row}${turn.square.col}`}>
+                {turn.player} selected {turn.square.row},{turn.square.col}
+            </li>)}
+        </ol>
+    );
+}
